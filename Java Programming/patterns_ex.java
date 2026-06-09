2f114555b28467f501477e5dc240c3e361e5ec3b
Mohnish Shandilya <mohnishshandilya.3000@gmail.com>
2026-06-09 10:47:28 +0530

    Java Programming Practice

--- Java Programming/patterns_ex.java
+++ Java Programming/patterns_ex.java
@@ -31,6 +31,23 @@ public class patterns_ex {
             System.out.println();
         }
 
+        // To print the palindromic pyramid
+
+        for(int i=1; i<=row; i++) {
+            for(int j=1; j<=row-i; j++) {
+                System.out.print(" ");
+            }
+
+            for(int j=i; j>=1; j--) {
+                System.out.print(j);
+            }
+
+            for(int j=2; j<=i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
 
     }
 }
